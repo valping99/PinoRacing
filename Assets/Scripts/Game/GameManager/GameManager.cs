85e00f18138abe53e34be4bbc8be4972ee3d2a4f
WilIliam <long.le@indigames.net>
2022-06-20 12:11:18 +0700

    update music

--- Assets/Scripts/Game/GameManager/GameManager.cs
+++ Assets/Scripts/Game/GameManager/GameManager.cs
@@ -24,7 +24,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
     }
 
     // Update is called once per frame
