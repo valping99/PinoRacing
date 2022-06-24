518c4d312aa320e905ada10901b6cee5d3559aa5
datdiep754129 <dat.diep@indigames.net>
2022-06-24 09:54:24 +0700

    Delete old UI, Fix Animation Sticker color, Fix Effect Waterdrop

--- Assets/Scripts/UI/Waterdrop.cs
+++ Assets/Scripts/UI/Waterdrop.cs
@@ -7,15 +7,19 @@ public class Waterdrop : MonoBehaviour
     public GameObject waterDrop;
     private UIManager managers;
     public Animator animWaterDrop;
+    public CharacterInputController charInput;
 
     public float timerToDrip;
 
     const string k_AnimDropWater = "WaterDrop";
+    const string k_AnimStun = "Stun";
     public bool enableAnim = false;
     // Start is called before the first frame update
     void Start()
     {
         managers = FindObjectOfType<UIManager>();
+        waterDrop.gameObject.SetActive(false);
+        charInput = FindObjectOfType<CharacterInputController>();
     }
 
     // Update is called once per frame
@@ -30,10 +34,15 @@ public class Waterdrop : MonoBehaviour
         {
             enableAnim = false;
         }
+        if(managers.timeValueUp >= timerToDrip + 60f)
+        {
+            animWaterDrop.speed += 0.005f * Time.deltaTime;
+        }
     }
 
     void CheckAnimation()
     {
         animWaterDrop.SetBool(k_AnimDropWater, enableAnim);
+        animWaterDrop.SetBool(k_AnimStun, charInput.m_Stuns);
     }
 }
