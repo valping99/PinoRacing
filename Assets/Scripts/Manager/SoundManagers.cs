a212965158b5f6554608ff94b70f2bbd3c9cc1b9
WilIliam <long.le@indigames.net>
2022-07-06 14:05:53 +0700

    Refactor Sound Manager with Command Pattern

--- Assets/Scripts/Manager/SoundManagers.cs
+++ Assets/Scripts/Manager/SoundManagers.cs
@@ -26,15 +26,18 @@ public class SoundManagers : MonoBehaviour
     public AudioClip itemIndication;
     public AudioClip rankDisplay;
 
+    private Command hit,stop,loop;
+
     #endregion
     #region Unity Method
     void Start()
     {
         GetResourcesAudio();
     }
+
     #endregion
     #region Class
-    [Tooltip("Get Audio in Resources")]
+
     void GetResourcesAudio()
     {
         tapSE = Resources.Load<AudioClip>("Audio/No1");
@@ -49,75 +52,54 @@ public class SoundManagers : MonoBehaviour
         rankDisplay = Resources.Load<AudioClip>("Audio/No10");
         dashBoardSE = Resources.Load<AudioClip>("Audio/No11");
         stingPickSE = Resources.Load<AudioClip>("Audio/No12");
+
+        hit = new PlaySound();
+        stop = new StopSound();
+        loop = new PlayLoopSound();
     }
 
-    [Tooltip("Set Audio")]
     public void PlaySound(string clip)
     {
         switch (clip)
         {
             case "Tap":
-                audio_source.clip = tapSE;
-                audio_source.PlayOneShot(tapSE, 0.6f);
+                hit.Execute(audio_source,tapSE,0.6f);
                 break;
             case "TopScreen":
-                audio_source.loop = true;
-                audio_source.clip = topScreen;
-                audio_source.volume = 0.2f;
-                audio_source.Play();
-                //audio_source.PlayOneShot(topScreen, 0.2f);
-                //StartCoroutine(ReSound("TopScreen"));
+                loop.Execute(audio_source,topScreen,0.2f);
                 break;
             case "CountDown":
-                audio_source.clip = countDownSE;
-                audio_source.PlayOneShot(countDownSE, 0.6f);
+                hit.Execute(audio_source,countDownSE,0.6f);
                 break;
             case "No4":
-                audio_source.clip = itemSE;
-                audio_source.PlayOneShot(itemSE, 0.2f);
+                hit.Execute(audio_source,itemSE,0.2f);
                 break;
             case "No5":
-                audio_source.clip = damageSE;
-                audio_source.PlayOneShot(damageSE, 0.6f);
+                hit.Execute(audio_source,damageSE,0.6f);
                 break;
             case "Finish":
-                audio_source.clip = finishSE;
-                audio_source.PlayOneShot(finishSE, 0.6f);
+                hit.Execute(audio_source,finishSE,0.6f);
                 break;
             case "BGM":
-                audio_source.loop = true;
-                audio_source.clip = gameBGM;
-                audio_source.volume = 0.2f;
-                audio_source.Play();
-                //audio_source.PlayOneShot(gameBGM, 0.2f);
-                //StartCoroutine(ReSound("BGM"));
+                loop.Execute(audio_source,gameBGM,0.2f);
                 break;
             case "Clear":
-                audio_source.loop = true;
-                audio_source.clip = clearBGM;
-                audio_source.volume = 0.2f;
-                audio_source.Play();
-                //audio_source.PlayOneShot(clearBGM, 0.2f);
-                //StartCoroutine(ReSound("Clear"));
+                loop.Execute(audio_source,clearBGM,0.2f);
                 break;
             case "Indication":
-                audio_source.clip = itemIndication;
-                audio_source.PlayOneShot(itemIndication, 0.6f);
+                hit.Execute(audio_source,itemIndication,0.6f);
                 break;
             case "Rank":
-                audio_source.clip = rankDisplay;
-                audio_source.PlayOneShot(rankDisplay, 0.6f);
+                hit.Execute(audio_source,rankDisplay,0.6f);
                 break;
             case "No11":
-                audio_source.clip = dashBoardSE;
-                audio_source.PlayOneShot(dashBoardSE, 0.6f);
+                hit.Execute(audio_source,dashBoardSE,0.6f);
                 break;
             case "Stop":
-                audio_source.Stop();
+                stop.Execute(audio_source,null,0);
                 break;
             case "No12":
-                audio_source.clip = stingPickSE;
-                audio_source.PlayOneShot(stingPickSE, 1f);
+                hit.Execute(audio_source,stingPickSE,1f);
                 break;
         }
     }
@@ -127,10 +109,5 @@ public class SoundManagers : MonoBehaviour
         PlaySound("Tap");
     }
 
-    IEnumerator ReSound(string sound)
-    {
-        yield return new WaitUntil(() => audio_source.isPlaying == false);
-        PlaySound(sound);
-    }
     #endregion
 }
