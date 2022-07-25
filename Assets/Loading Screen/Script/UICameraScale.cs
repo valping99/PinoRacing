cd1617f6469e5aaa0509f486100f8d89c8b127af
WilIliam <long.le@indigames.net>
2022-07-25 09:28:08 +0700

    + Fix the speed when hitting another ice + Add loading scenes with Addressables

--- Assets/Loading Screen/Script/UICameraScale.cs
+++ Assets/Loading Screen/Script/UICameraScale.cs
@@ -2,52 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Baccarat
+/// <summary>  
+/// This class is used to scale the UI camera.
+/// </summary>
+[ExecuteInEditMode, RequireComponent(typeof(Camera))]
+public class UICameraScale : MonoBehaviour
 {
-    /// <summary>  
-    /// This class is used to scale the UI camera.
-    /// </summary>
-    [ExecuteInEditMode, RequireComponent(typeof(Camera))]
-    public class UICameraScale : MonoBehaviour
+
+    #region Variables
+
+    public Camera targetCamera;
+    public Vector2Int screenReferenceSize = new Vector2Int(1920, 1080);
+    public float baseCameraSize = 8;
+
+
+    #endregion
+
+    #region Unity Methods
+
+    private void Reset()
     {
-
-        #region Variables
-
-        public Camera targetCamera;
-        public Vector2Int screenReferenceSize = new Vector2Int(1920, 1080);
-        public float baseCameraSize = 8;
-
-
-        #endregion
-
-        #region Unity Methods
-
-        private void Reset()
-        {
-            this.targetCamera = this.GetComponent<Camera>();
-        }
-
-        private IEnumerator Start()
-        {
-            yield return true;
-            Update();
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-            if (this.screenReferenceSize.x <= 0 || this.screenReferenceSize.y <= 0)
-                return;
-
-            var screen_ratio = Screen.width * 1.0f / Screen.height;
-            var ratio = this.screenReferenceSize.x * 1.0f / this.screenReferenceSize.y;
-
-            if (ratio < screen_ratio)
-                this.targetCamera.orthographicSize = this.baseCameraSize;
-            else
-                this.targetCamera.orthographicSize = this.baseCameraSize * ratio / screen_ratio;
-        }
-        #endregion
-
+        this.targetCamera = this.GetComponent<Camera>();
     }
+
+    private IEnumerator Start()
+    {
+        yield return true;
+        Update();
+    }
+    void Update()
+    {
+        if (this.screenReferenceSize.x <= 0 || this.screenReferenceSize.y <= 0)
+            return;
+
+        var screen_ratio = Screen.width * 1.0f / Screen.height;
+        var ratio = this.screenReferenceSize.x * 1.0f / this.screenReferenceSize.y;
+
+        if (ratio < screen_ratio)
+            this.targetCamera.orthographicSize = this.baseCameraSize;
+        else
+            this.targetCamera.orthographicSize = this.baseCameraSize * ratio / screen_ratio;
+    }
+    #endregion
+
 }
